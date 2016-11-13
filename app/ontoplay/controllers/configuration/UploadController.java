c566a6e250c6ae1267eca0d1ea03f5026984d1ad
Motasem Alwazir <almotasem89@gmail.com>
2016-11-13 13:00:58 +0100

    Move configuration classes and utils to the Ontoplay

---
+++ app/ontoplay/controllers/configuration/UploadController.java
@@ -0,0 +1,58 @@
+package controllers.configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+
+import ontoplay.OntologyHelper;
+import ontoplay.Pathes;
+
+
+import controllers.configuration.utils.OntoplayOntologyUtils;
+import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Result;
+
+public class UploadController extends Controller {
+	public static Result showUploadPage() {
+		return ok(views.html.configuration.upload.render(OntologyHelper.ontologyName,OntologyHelper.iriString));
+	}
+
+	public static Result upload() {
+		MultipartFormData body = request().body().asMultipartFormData();
+		FilePart<File> ontologyFile = body.getFile("ontologyFile");
+		String result = "";
+		if (ontologyFile != null) {
+			result += "file Name " + ontologyFile.getFilename()+"\n";
+			result += "contentType " + ontologyFile.getContentType()+"\n";
+		
+			//File file = ontologyFile.getFile();
+			File file = ontologyFile.getFile();
+		    File destination = new File(Pathes.UPLOADS_PATH, ontologyFile.getFilename());
+		    destination.delete();
+		    try {
+				FileUtils.moveFile(file, destination);
+			} catch (IOException e) {
+				flash("error", "Missing file");
+
+				// return redirect(routes.Application.index());
+				return ok("Error");
+			}
+		      Map<String,String[]> dataPart = request().body().asMultipartFormData().asFormUrlEncoded();
+	          String uri = dataPart.get("ontologyIRI")[0];
+
+			OntoplayOntologyUtils.setOntologyInTheXml(ontologyFile.getFilename(),uri);
+			OntoplayOntologyUtils.setOntologyCF();
+			OntoplayOntologyUtils.resetAnnotationCF();
+			return ok("File uploaded \n "+result+"\n URI "+uri);
+		} else {
+			flash("error", "Missing file");
+
+			// return redirect(routes.Application.index());
+			return ok("Error");
+		}
+	}
+}
---
+++ app/ontoplay/controllers/configuration/utils/OntoplayOntologyUtils.java
@@ -0,0 +1,68 @@
+package controllers.configuration.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import ontoplay.OntologyHelper;
+import ontoplay.Pathes;
+
+/**
+ * Read and set ontology configuration.
+ * @author Motasem Alwazir
+ *
+ */
+public class OntoplayOntologyUtils {
+
+	public static void setOntologyInTheXml(String newOntologyName, String iri) {
+			
+		   try {
+				XMLUtils xmlUtils = new XMLUtils(Pathes.ONTOLOGY_XML_FILE_PATH);
+				NodeList nodeList = xmlUtils.getElementsbyName("ontology");
+				Element element = (Element)nodeList.item(0);
+				element.setAttribute("name", newOntologyName);
+				element.setAttribute("iri", iri);
+				xmlUtils.prettyPrint();
+		   }catch(Exception e){
+			   System.out.println("Error "+e.toString());
+			   e.printStackTrace(System.out);
+		   }
+
+	}
+
+	public static void setOntologyCF() {
+		//we need to read the ontology name from the XML CF
+		try {
+
+			XMLUtils xmlUtils = new XMLUtils(Pathes.ONTOLOGY_XML_FILE_PATH);
+			NodeList nodeList = xmlUtils.getElementsbyName("ontology");
+			Element element = (Element)nodeList.item(0);
+			String ontologyName=element.getAttribute("name");
+			String ontologyIri=element.getAttribute("iri");
+			OntologyHelper.ontologyName=ontologyName;
+			OntologyHelper.fileName = Pathes.UPLOADS_PATH+ontologyName;
+			OntologyHelper.file = "file:"+Pathes.UPLOADS_PATH+ontologyName;
+			OntologyHelper.iriString=ontologyIri;
+			OntologyHelper.nameSpace=ontologyIri+"#";
+		}catch(Exception e){
+			
+		}
+	}
+	/**
+	 * To replace the current annotation configuration with the original (empty and structured) one
+	 */
+	public static void resetAnnotationCF() {
+		File original=new File(Pathes.Annotation_xml_original);
+		File current =new File(Pathes.Annotation_XML_FILE_PATH);
+		current.delete();
+		 try {
+				FileUtils.moveFile(original, current);
+			} catch (IOException e) {
+				System.out.println("Error replacing ontology");
+			}
+	}
+
+}
