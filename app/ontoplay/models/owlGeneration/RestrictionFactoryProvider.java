0810afee3f84157c5b557f6823be8f1622400337
drozd <drozdowicz@gmail.com>
2017-03-02 23:58:43 +0100

    Fixed an issue with injecting RestrictionFactory

--- app/ontoplay/models/owlGeneration/RestrictionFactoryProvider.java
+++ app/ontoplay/models/owlGeneration/RestrictionFactoryProvider.java
@@ -3,6 +3,7 @@ package ontoplay.models.owlGeneration;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
+import com.google.inject.util.Types;
 import ontoplay.models.ConfigurationException;
 import ontoplay.models.PropertyValueCondition;
 
@@ -22,6 +23,8 @@ public class RestrictionFactoryProvider {
     }
 
     public <U extends PropertyValueCondition> RestrictionFactory<U> getRestrictionFactory(U condition) throws ConfigurationException {
-        return injector.getInstance(Key.get(new TypeLiteral<RestrictionFactory<U>>(){}));
+        //return (PropertyConditionRenderer<U>) injector.getInstance(Key.get(TypeLiteral.get(Types.newParameterizedType(PropertyConditionRenderer.class, property.getClass()))));
+        return (RestrictionFactory<U>) injector.getInstance(Key.get(TypeLiteral.get(Types.newParameterizedType(RestrictionFactory.class, condition.getClass()))));
+//        return injector.getInstance(Key.get(new TypeLiteral<RestrictionFactory<U>>(){}));
     }
 }
